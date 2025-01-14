6b494570a2b6f753d6f376205fe1e14a7cdb04ec
CHATHURANGA KHD <144873531+Deneth0077@users.noreply.github.com>
2025-01-15 00:22:32 +0530

    Update MyApps.jsx

--- src/pages/miniComponents/MyApps.jsx
+++ src/pages/miniComponents/MyApps.jsx
@@ -220,7 +220,7 @@ const MyApps = () => {
   useEffect(() => {
     const getMyApps = async () => {
       const { data } = await axios.get(
-        "https://portfolio-main-lemon-gamma.vercel.app/api/v1/softwareapplication/getall",
+        "https://backend-folio-ten.vercel.app/api/v1/softwareapplication/getall",
         { withCredentials: true }
       );
       setApps(data.softwareApplications);
